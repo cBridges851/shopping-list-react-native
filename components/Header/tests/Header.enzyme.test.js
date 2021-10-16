39cbc1085a16c5c6bf651fb24f4595b117c113c0
Christa Bridges <christa@pebblepad.co.uk>
2021-10-16 16:45:26 +0100

    unit-tests Added snapshot test for the Header component

--- components/Header/tests/Header.enzyme.test.js
+++ components/Header/tests/Header.enzyme.test.js
@@ -4,21 +4,21 @@
 
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
-import Enzyme, { ReactWrapper, mount } from "enzyme";
+import Enzyme, { mount } from "enzyme";
 import { Header } from "../Header";
-import renderer from 'react-test-renderer';
-
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("Using the Header component", () => {
     let props = {
         title: "Title With a Head"
     };
-    let wrapper = mount(<Header title={props.title}/>);
+
+    let wrapper = mount(<Header title={props.title}/>)
 
     test("it renders correctly", () => {
-        renderer.create(<Header title={props.title}/>)
-    })
+        expect(wrapper).toMatchSnapshot()
+    });
+
     describe("given the title 'Title With a Head'", () => {
         test("then it should render this title", () => {
             let header = wrapper.find("Header");
@@ -39,3 +39,4 @@ describe("Using the Header component", () => {
         })
     });
 });
+
