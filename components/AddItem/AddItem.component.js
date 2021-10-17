760eced6c0fd6568f4af5ad93652a28eb34ee956
Christa Bridges <christa@pebblepad.co.uk>
2021-10-17 16:22:58 +0100

    refactors CustomButton is now a component.

--- components/AddItem/AddItem.component.js
+++ components/AddItem/AddItem.component.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/dist/FontAwesome";
+import { CustomButton } from "../CustomButton/CustomButton.component";
 import { TextInputBox } from "../TextInputBox/TextInputBox.component";
 
 const AddItem = ({addItem}) => {
@@ -13,30 +14,12 @@ const AddItem = ({addItem}) => {
     return (
         <View>
             <TextInputBox onChangeText={onChange}></TextInputBox>
-            <TouchableOpacity 
-                style={styles.addItemButton}
-                onPress={() => addItem(text)}>
-                <Icon style={styles.addItemButtonText} name="plus"></Icon>
-                <Text style={styles.addItemButtonText}>Add</Text>
-            </TouchableOpacity>
+            <CustomButton 
+                iconName="plus"
+                buttonText="Add" 
+                onPressButton={() => addItem(text)}/>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    addItemButton: {
-        backgroundColor: "#A6CCED",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 20,
-        padding: 5
-    },
-    addItemButtonText: {
-        color: "#003B6E",
-        fontSize: 20,
-        marginRight: 3
-    }
-});
 
 export default AddItem;
---
+++ components/CustomButton/CustomButton.component.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/dist/FontAwesome";
+
+export class CustomButton extends Component {
+    render() {
+        return (
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={this.props.onPressButton}>
+                <Icon style={styles.buttonText} name={this.props.iconName}></Icon>
+                <Text style={styles.buttonText}>{this.props.buttonText}</Text>
+            </TouchableOpacity>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "#A6CCED",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 20,
+        padding: 5
+    },
+    buttonText: {
+        color: "#003B6E",
+        fontSize: 20,
+        marginRight: 3
+    }
+});
